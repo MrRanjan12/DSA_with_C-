28d73065f204db1f2d87573b42eb791d361d7d14
Ranjan kumar <136085563+MrRanjan12@users.noreply.github.com>
2024-09-25 16:28:30 +0530

    Update SwapMaxMin_inArr.cpp
    This C++ program swaps the minimum and maximum values in an integer array. It first finds the indices of the minimum and maximum elements using a loop, then swaps these values in the array. After the swap, the updated array is printed to the console. The program uses standard functions like std::min() and std::max() from the <algorithm> library, and handles initialization with INT32_MAX and INT32_MIN from <climits> for compatibility with older C++ versions.

--- SwapMaxMin_inArr.cpp
+++ SwapMaxMin_inArr.cpp
@@ -1,1 +1,47 @@
+#include <iostream> 
+#include <algorithm> // For std::max and std::min
+#include <climits>   // For INT32_MAX and INT32_MIN // for only old version c++
+using namespace std;
 
+int swapMaxMin(int arr[], int size){
+  int minValue = INT32_MAX;
+  int maxValue = INT32_MIN;
+  int minValueIndex = 0;
+  int maxValueIndex = 0;
+  for (int i = 0; i < size; i++)
+  {
+     minValue = min(arr[i],minValue);
+     maxValue = max(arr[i],maxValue);
+     if (minValue == arr[i]){
+       minValueIndex = i;
+     }
+     if (maxValue == arr[i])
+     {
+       maxValueIndex = i;
+     }
+     
+  }
+  // cout << maxValue;
+  swap(arr[minValueIndex],arr[maxValueIndex]);
+  for (int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  return 0;
+}
+
+
+int main(){
+  int size = 7;
+  int arr[] = {12,63,10,14,32,34,89};
+  cout << "original value of arr[] : ";
+  for (int i = 0; i < size; i++)
+  {
+     cout << arr[i] << " ";
+  }
+  cout << endl;
+  cout << " swapped value of an arr[] : ";
+  swapMaxMin(arr, size);
+
+  return 0;
+}
